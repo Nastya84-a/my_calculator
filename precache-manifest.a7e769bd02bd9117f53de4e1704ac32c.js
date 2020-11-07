650ef42b9292e2980f0fae8123b01ebaee8fcd03
Anastasya Kushnir <anastasyakyshnir@gmail.com>
2020-11-07 10:59:28 +0200

    Updates

---
+++ precache-manifest.a7e769bd02bd9117f53de4e1704ac32c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac39e6611c28429d9018639cdd509088",
+    "url": "/my_calculator/index.html"
+  },
+  {
+    "revision": "8f53e59bf36a984c7db1",
+    "url": "/my_calculator/static/css/2.5861e1ce.chunk.css"
+  },
+  {
+    "revision": "d3058290c9ad298be2fc",
+    "url": "/my_calculator/static/css/main.9c8ed6f1.chunk.css"
+  },
+  {
+    "revision": "8f53e59bf36a984c7db1",
+    "url": "/my_calculator/static/js/2.c5636010.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my_calculator/static/js/2.c5636010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3058290c9ad298be2fc",
+    "url": "/my_calculator/static/js/main.72af36ef.chunk.js"
+  },
+  {
+    "revision": "3808fe6cd2e1480d28b0",
+    "url": "/my_calculator/static/js/runtime-main.b1292df9.js"
+  }
+]);
